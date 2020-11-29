de3178a9c78894c4ff93ebd73b092ba601826c78
Mugilan <mugilancodes@gmail.com>
2020-11-29 13:20:48 +0530

    Schema Completed? 😩

--- init.sql
+++ init.sql
@@ -23,9 +23,11 @@ CREATE TABLE IF NOT EXISTS `student` (
 
 CREATE TABLE IF NOT EXISTS `marks` (
   `register_no` VARCHAR(15) PRIMARY KEY,
-  `cgpa`,
-  `active_backlog`,
-  `backlog_history`,
+  `cgpa` NUMERIC(4,2) NOT NULL 
+    CHECK(`cgpa` >= 0 
+      AND `cgpa` <= 10),
+  `active_backlog` INT DEFAULT 0,
+  `backlog_history` INT DEFAULT 0,
   FOREIGN KEY (`register_no`) 
     REFERENCES `student`(`register_no`) 
       ON DELETE CASCADE
@@ -33,12 +35,18 @@ CREATE TABLE IF NOT EXISTS `marks` (
 
 CREATE TABLE IF NOT EXISTS `education` (
   `register_no` VARCHAR(15) PRIMARY KEY,
-  `10th_board`,
-  `10th_percentage`,
-  `12th_board`,
-  `12th_percentage`,
-  `grad_course`,
-  `grad_percentage`,
+  `10th_board` VARCHAR(30) NOT NULL,
+  `10th_percentage` NUMERIC(4, 2) NOT NULL 
+    CHECK(`10th_percentage` >= 0 
+      AND `10th_percentage` <= 100),
+  `12th_board` VARCHAR(30) NOT NULL,
+  `12th_percentage` NUMERIC(4, 2) NOT NULL 
+    CHECK(`12th_percentage` >= 0 
+      AND `12th_percentage` <= 100),
+  `grad_course` VARCHAR(30),
+  `grad_percentage` NUMERIC(4, 2) 
+    CHECK(`grad_percentage` >= 0 
+      AND `grad_percentage` <= 100),
   FOREIGN KEY (`register_no`)
     REFERENCES `student`(`register_no`)
       ON DELETE CASCADE
