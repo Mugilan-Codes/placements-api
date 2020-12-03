e5021d4c4582002f6a3d81bf2af93ca101d83f0e
Mugilan <mugilancodes@gmail.com>
2020-12-03 09:30:29 +0530

    JOI Validator middleware

--- src/middleware/index.js
+++ src/middleware/index.js
@@ -1,2 +1,3 @@
 export { default as logger } from './logger';
 export { default as errorHandler } from './error-handler';
+export { default as validate } from './schema-validator';
---
+++ src/middleware/schema-validator.js
@@ -0,0 +1,22 @@
+const validator = (schema, property = 'body') => {
+  return (req, res, next) => {
+    const { error, value } = schema.validate(req[property], {
+      abortEarly: false,
+    });
+
+    const isValid = error == null;
+
+    if (isValid) {
+      console.log({ value });
+      next();
+    } else {
+      const { details } = error;
+      const message = details.map((i) => i.message).join(',');
+
+      console.error('Validation Error -> ', message);
+      res.status(422).json({ error: message });
+    }
+  };
+};
+
+export default validator;
