43a7ac63481d9db76eb94b4578c9ab20be99aa82
Mugilan <mugilancodes@gmail.com>
2020-12-10 23:25:35 +0530

    Role Types as String

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,3 +1,4 @@
 export { default as ApiError } from './api-error';
 export { router } from './promise-router';
 export { default as bcryptPass } from './bcrypt-password';
+export { Role } from './types';
