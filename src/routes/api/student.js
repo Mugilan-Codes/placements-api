6289555c7170017b062a36c7f542205859ef22da
Mugilan <mugilancodes@gmail.com>
2020-12-25 20:56:47 +0530

    Rearrange routes order

--- src/routes/api/student.js
+++ src/routes/api/student.js
@@ -5,14 +5,14 @@ import { Student } from '../../controllers';
 
 router
   .get('/', authorize(Role.Student), Student.getDetail)
+  .post('/register', validate(schema.student.register), Student.register)
+  .post('/login', validate(schema.student.login), Student.login)
   .get('/courses', authorize(Role.Student), Student.getAllCourses)
   .get(
     '/:register_no',
     validate(schema.student.registerParam, 'params'),
     Student.getOne
   )
-  .post('/register', validate(schema.student.register), Student.register)
-  .post('/login', validate(schema.student.login), Student.login)
   .post('/add/marks', validate(schema.marks.add), Student.addMarks);
 
 export default router;
